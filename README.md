e6e61777bed8ee4471228c875e8cba54274f1cb4
gynflo <ginet.florian.dev@gmail.com>
2024-01-21 12:51:47 +0100

    Complete Login Form component with schema zod logic

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 - Modification de la page d'acceuil
 - Création du bouton "login-button" avec la possibilité de changer le comportement de ce dernier, soit en modal ou redirect
 - Création de la login Page
+  - Création du loginForm (CS)
+  - Création du CardWrapper qui prends en props (headerLabel, backButtonLabel, backButtonHref,shwoSocial) (CC)
+  - Le composant CardWrapper fait appel aux composant de shadcn-ui (Card, CardContent, CardHeader, CardFooter) (CC)
+  - Création des boutton sociaux ()
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,37 @@
+"use client";
+
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { LoginSchema } from "@/schemas";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  /* Soumission du formulaire */
+  const onSubmit = (data: z.infer<typeof LoginSchema>) => {
+    console.log(data);
+  };
+
   return (
     <CardWrapper
       headerLabel="Welcome back"
@@ -8,7 +39,45 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login Form{" "}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="john.doe@example.email.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Password */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          {/* Sumit Buuton */}
+          <Button className="w-full">Login</Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
